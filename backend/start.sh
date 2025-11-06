ee67c79abc0bbe514e7c884504445f0c81a6edfa
programador.ecu <programador.ecu@gmail.com>
2025-11-06 15:45:53 -0500

    NUEVO ARCHIVO START.SH PARA CONFIG DEL DOCKER EN RENDER

---
+++ backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Este es nuestro "script de arranque"
+
+# Paso 1: Llamar al "constructor de estantes" (Alembic)
+echo "--- Iniciando migración de la base de datos (Alembic)..."
+python -m alembic upgrade head
+
+# Paso 2: Encender la "oficina" (Uvicorn)
+echo "--- Migración completa. Iniciando el servidor (Uvicorn)..."
+python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
